5346e7a2e05b8673a3a5496db12300d3dad06bf1
Nithin <nithinpaddyur@gmial.com>
2024-10-04 18:17:00 +0530

    first commit

---
+++ src/test/java/com/nithin/testLinks/FindBrokenlinks.java
@@ -0,0 +1,58 @@
+package com.nithin.testLinks;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FindBrokenlinks {
+	
+	public class FindBrokenLinks {
+		WebDriver driver = null;
+		
+		@Test
+		public void findBrokenLinks() {
+			
+//			All the links on the web page
+			
+			List<WebElement> links = driver.findElements(By.tagName("a"));
+			System.out.println("No of links are: "+links.size());
+			
+			for(WebElement link:links) {
+//				System.out.println(link.getText());
+				String linkURL = link.getAttribute("href");
+				
+				VerifyLinkStatusCode.verifylinks(linkURL);
+			}
+			System.out.println("Total number of invalid links are");
+			VerifyLinkStatusCode.getInvalidLinkCount();
+			
+			
+		}
+		
+		
+
+		@BeforeTest
+		public void init() {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			driver.get("https://edition.cnn.com/");
+		}
+
+		@AfterTest
+		public void tearDown() {
+
+		}
+
+	}
+}
---
+++ src/test/java/com/nithin/testLinks/VerifyLinkStatusCode.java
@@ -0,0 +1,38 @@
+package com.nithin.testLinks;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class VerifyLinkStatusCode {
+	public static int invalidLinkCount;
+
+	public static void verifylinks(String link) {
+		if (link != null && !link.isEmpty()) {
+			try {
+				URL url = new URL(link);
+				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//			
+//			Set Timeout
+				connection.setConnectTimeout(3000);
+//	Set request method to HEAD to check only the headers not the entire content
+				connection.setRequestMethod("HEAD");
+
+				connection.connect();
+
+				if (connection.getResponseCode() == 200) {
+
+				} else {
+					invalidLinkCount++;
+				}
+
+			} catch (Exception e) {
+
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void getInvalidLinkCount() {
+		System.out.println("Total invalid links are: " + invalidLinkCount);
+	}
+}
